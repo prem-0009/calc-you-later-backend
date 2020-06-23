b1d27c6b43dc7d704200640832f91380e39b273f
prem <prem.sherpa@codeimmersives.com>
2020-06-22 23:04:58 -0400

    test cases calculator if/switch

--- calculate-back-end.js
+++ calculate-back-end.js
@@ -1,6 +1,60 @@
 /******************
  * YOUR CODE HERE *
  ******************/
+function calculate (str11, str22, str3){
+  let str1 = Number(str11);
+  let str2 = Number(str22);
+  
+  
+  // if ( str3 === '+' || str3 === 'plus' || str3 === 'added to'){
+  //   return str1 + str2;
+  // }
+  // else if ( str3 === '-' || str3 === 'minus' || str3 === 'subtracted from'){
+  //   return str1 - str2;
+  // }
+  // else if ( str3 === 'x' || str3 === 'X' || str3 === 'times' || str3 === 'multiplied by'){
+  //   return str1 * str2;
+  // }
+  // else if ( str3 === '/' || str3 === 'divided by' ){
+  //   return str1 / str2;
+  // }
+  // else if ( str3 === '%' || str3 === 'modulus' || str3 === 'mod'){
+  //   return str1 % str2;
+  // }
+  // else {
+  //   return `Sorry, that's not a mathematical operation!`
+  // }
+
+  //switch
+  switch(str3){
+    case '+':
+    case 'plus':
+    case 'added to':
+      return str1 + str2;
+    case '-':
+    case 'minus':
+    case 'subtracted from':
+      return str1 - str2;
+    case 'x':
+    case 'X':
+    case 'multiplied by':
+    case 'times':
+      return str1 * str2;
+    case '/':
+    case 'divided by':
+    case 'added':
+      return str1 / str2;
+    case '%':
+    case 'modulus':
+    case 'mod':
+      return str1 % str2;
+    default:
+      return `Sorry, that's not a mathematical operation!`;
+
+  }
+
+
+}
 
 
  
